7cfa6a2cd37055db6de8791fbd0423f5dadb2746
awdesh <awdesh.sharma1@gmail.com>
2016-07-26 22:11:50 -0700

    InheritanceVsComposition

---
+++ EffectiveJava/src/main/java/InheritanceVsComposition/AbstractPerson.java
@@ -0,0 +1,17 @@
+package InheritanceVsComposition;
+
+/**
+ * Created by awdesh on 7/25/16.
+ */
+public abstract class AbstractPerson {
+
+    public abstract String getOccupation();
+
+    public abstract int getAge();
+
+    public abstract String getName();
+
+    public abstract String getGender();
+}
+
+
---
+++ EffectiveJava/src/main/java/InheritanceVsComposition/Client.java
@@ -0,0 +1,9 @@
+package InheritanceVsComposition;
+
+/**
+ * Created by awdesh on 7/25/16.
+ */
+public class Client{
+    ExendingPersonClass person = new ExendingPersonClass("Jonh", 25, "male", "Student");
+
+}
